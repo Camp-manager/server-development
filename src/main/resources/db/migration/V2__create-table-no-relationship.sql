8e641ff252d49105245e4994d51511a8c0eff5bb
Uryel <uryeljodelucca18@gmail.com>
2025-05-08 21:00:58 -0300

    feat: implementar as tabelas sem relacionamento no Flyway.

---
+++ src/main/resources/db/migration/V2__create-table-no-relationship.sql
@@ -0,0 +1,39 @@
+CREATE TABLE endereco(
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    rua TEXT NOT NULL,
+    numero TEXT NOT NULL,
+    cidade TEXT NOT NULL,
+    bairro TEXT NOT NULL,
+    ponto_referencia TEXT NOT NULL
+);
+
+CREATE TABLE medicamento(
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    nome TEXT NOT NULL,
+    quantidade TEXT NOT NULL,
+    tipo VARCHAR(1) NOT NULL,
+    valor NUMERIC(10,2)
+);
+
+CREATE TABLE cronograma(
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    data_inicial VARCHAR(10) NOT NULL,
+    data_final VARCHAR(10) NOT NULL,
+    descricao TEXT
+);
+
+CREATE TABLE carteirinha(
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    texto_apresentacao TEXT NOT NULL
+);
+
+CREATE TABLE carteirinha(
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    descricao TEXT NOT NULL,
+    design TEXT NOT NULL,
+    preco_camiseta NUMERIC(10,2),
+    preco_acampamento NUMERIC(10,2),
+);
+
+
