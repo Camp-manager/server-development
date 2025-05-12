c6906ef17ebb803d5f164d12592356df1445e436
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-12 12:03:27 -0300

    refactor: alterar a forma como a tabela de acampamento e estoque se relacionam

--- src/main/resources/db/migration/V06__create-table-acampamento.sql
+++ src/main/resources/db/migration/V06__create-table-acampamento.sql
@@ -5,10 +5,8 @@ CREATE TABLE acampamento(
     codigo_registro VARCHAR(6) NOT NULL,
 
     id_tema INTEGER NOT NULL,
-    id_estoque INTEGER NOT NULL,
     id_tipo_acampamento INTEGER NOT NULL,
 
     CONSTRAINT fk_acampamento_tema FOREIGN KEY (id_tema) REFERENCES tema(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_acampamento_estoque FOREIGN KEY (id_estoque) REFERENCES estoque(id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_acampamento_tipo_acampamento FOREIGN KEY (id_tipo_acampamento) REFERENCES tipo_acampamento(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
--- src/main/resources/db/migration/V16__create-table-muitos-para-muitos.sql
+++ src/main/resources/db/migration/V16__create-table-muitos-para-muitos.sql
@@ -6,3 +6,12 @@ CREATE TABLE pessoa_medicamento_controller(
     CONSTRAINT fk_pessoa_medicamento_controller_pessoa FOREIGN KEY (id_pessoa)REFERENCES pessoa(id)ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_pessoa_medicamento_controller_medicamento FOREIGN KEY (id_medicamento)REFERENCES medicamento(id)ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+CREATE TABLE acampamento_estoque_controller(
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    id_acampamento INTEGER NOT NULL,
+    id_estoque INTEGER NOT NULL,
+
+    CONSTRAINT fk_acampamento_estoque_controller_acampamento FOREIGN KEY (id_acampamento)REFERENCES acampamento(id)ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_acampamento_estoque_controller_estoque FOREIGN KEY (id_estoque)REFERENCES estoque(id)ON DELETE RESTRICT ON UPDATE CASCADE
+);
