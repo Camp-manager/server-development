0ad683b4e0461574806dc978c8323f76f82fa8c0
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-12 09:48:58 -0300

    refactor: adicionar o @Repository nos respositorios de Carteirinha e Medicamento

--- src/main/java/com/camp/manager/infra/persistence/repository/CarteirinhaRepository.java
+++ src/main/java/com/camp/manager/infra/persistence/repository/CarteirinhaRepository.java
@@ -2,6 +2,8 @@ package com.camp.manager.infra.persistence.repository;
 
 import com.camp.manager.infra.persistence.entity.CarteirinhaEntityJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarteirinhaRepository extends JpaRepository<CarteirinhaEntityJpa, Long> {
 }
--- src/main/java/com/camp/manager/infra/persistence/repository/MedicamentoRepository.java
+++ src/main/java/com/camp/manager/infra/persistence/repository/MedicamentoRepository.java
@@ -2,6 +2,8 @@ package com.camp.manager.infra.persistence.repository;
 
 import com.camp.manager.infra.persistence.entity.MedicamentoEntityJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicamentoRepository extends JpaRepository<MedicamentoEntityJpa, Long> {
 }
