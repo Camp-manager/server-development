954629cbcf6ba8fbe5646bb43f67fa3e5005139f
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-12 12:45:01 -0300

    feat: implementar a entidade de Cronograma juntamente com seu repository na camada de infra

---
+++ src/main/java/com/camp/manager/infra/persistence/entity/CronogramaEntityJpa.java
@@ -0,0 +1,34 @@
+package com.camp.manager.infra.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cronograma", schema = "public")
+public class CronogramaEntityJpa {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_inicial")
+    private String dataInicio;
+
+    @Column(name = "data_final")
+    private String dataFinal;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_acampamento")
+    private AcampamentoEntityJpa acampamento;
+}
