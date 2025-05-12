55dd83245383a466c1c504e98d0551f8ba90886a
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-12 12:25:25 -0300

    feat: implementar a entidade de Acampamento juntamente com seu repository na camada de infra

---
+++ src/main/java/com/camp/manager/infra/persistence/entity/AcampamentoEntityJpa.java
@@ -0,0 +1,38 @@
+package com.camp.manager.infra.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "acampamento", schema = "public")
+public class AcampamentoEntityJpa {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "limite_campistas")
+    private Long limiteCampistas;
+
+    @Column(name = "limite_funcionario")
+    private Long limiteFuncionario;
+
+    @Column(name = "codigo_registro")
+    private String codigoRegistro;
+
+    @OneToOne
+    @JoinColumn(name = "id_tema")
+    private TemaEntityJpa tema;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipo_acampamento")
+    private TipoAcampamentoEntityJpa tipoAcampamento;
+}
---
+++ src/main/java/com/camp/manager/infra/persistence/repository/AcampamentoRepository.java
@@ -0,0 +1,9 @@
+package com.camp.manager.infra.persistence.repository;
+
+import com.camp.manager.infra.persistence.entity.AcampamentoEntityJpa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AcampamentoRepository extends JpaRepository<AcampamentoEntityJpa, Long> {
+}
