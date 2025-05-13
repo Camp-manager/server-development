c609395637ed72a31ea4a126d985205bcc1dd78e
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-13 10:52:24 -0300

    style: adicionar a descrição do tamanhoCamiseta.

--- src/main/java/com/camp/manager/domain/enums/TamanhoCamiseta.java
+++ src/main/java/com/camp/manager/domain/enums/TamanhoCamiseta.java
@@ -7,10 +7,18 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TamanhoCamiseta {
 
-    P("",""),
-    M("",""),
-    G("",""),
-    GG("","");
+    PP("PP","Extra Pequeno"),
+    P("P","Pequeno"),
+    M("M","Médio"),
+    G("G","Grande"),
+    GG("GG","Extra Grande"),
+    G1("G1","Extra Grande 1"),
+    G2("G2","Extra Grande 2"),
+    G3("G3","Extra Grande 3"),
+    G4("G4","Extra Grande 4"),
+    G5("G5","Extra Grande 5"),
+    G6("G6","Extra Grande 6"),
+    G7("G7","Extra Grande 7");
 
     private final String valor;
     private final String descricao;
