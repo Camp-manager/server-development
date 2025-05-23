b6458044890c2847c95e6c6cd5497e93c8152a9d
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-23 12:37:39 -0300

    fix: concertar o id repetido no insert de tipo_acampamento

--- src/main/resources/db/migration/V17__insert-table-tipo-acampamento.sql
+++ src/main/resources/db/migration/V17__insert-table-tipo-acampamento.sql
@@ -2,4 +2,4 @@ INSERT INTO tipo_acampamento (id, descricao, categoria) VALUES
 (1, 'Acampamento Mirim', 'C'),
 (2, 'Acampamento FAC', 'J'),
 (3, 'Acampamento Senior', 'A'),
-(3, 'Acampamento Casais', 'A');
+(4, 'Acampamento Casais', 'A');
