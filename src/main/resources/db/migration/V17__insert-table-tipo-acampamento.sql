efb264f2cbef76dc42ef15304432c0cd494d9388
Uryel Jó de Lucca Araujo de Oliveira <2311600443@fema.edu.br>
2025-05-23 11:17:47 -0300

    feat: implementar os dados pré-setados no flyway para as tabelas de tipo_acampamento e medicamento

---
+++ src/main/resources/db/migration/V17__insert-table-tipo-acampamento.sql
@@ -0,0 +1,5 @@
+INSERT INTO tipo_acampamento (id, descricao, categoria) VALUES
+(1, 'Acampamento Mirim', 'C'),
+(2, 'Acampamento FAC', 'J'),
+(3, 'Acampamento Senior', 'A'),
+(3, 'Acampamento Casais', 'A');
